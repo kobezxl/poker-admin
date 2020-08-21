1d003e063a1c3eb3f2ea4e3322394152e35541c1
zxl <1205653446@qq.com>
2020-08-22 02:00:43 +0800

    20200822-2

--- src/main/java/com/cn/poker/admin/modules/poker/controller/TaskController.java
+++ src/main/java/com/cn/poker/admin/modules/poker/controller/TaskController.java
@@ -3,7 +3,6 @@ package com.cn.poker.admin.modules.poker.controller;
 import com.cn.poker.admin.modules.poker.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -16,7 +15,7 @@ public class TaskController {
     /**
      *  新用户赠送策略包
      */
-    @Scheduled(cron = "*/5 * * * * ?")
+//    @Scheduled(cron = "*/5 * * * * ?")
     public void initialize(){
         taskService.newUserGiving();
     }
